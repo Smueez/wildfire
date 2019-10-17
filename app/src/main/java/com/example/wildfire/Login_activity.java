01ff61e3f8b15719bd125e8e4a4adc5daf73f263
Smueez <mueezsheersho95@gmail.com>
2019-10-17 22:26:37 +0600

    update3

---
+++ app/src/main/java/com/example/wildfire/Login_activity.java
@@ -0,0 +1,17 @@
+package com.example.wildfire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+
+
+
+    }
+}
--- app/src/main/java/com/example/wildfire/MainActivity.java
+++ app/src/main/java/com/example/wildfire/MainActivity.java
@@ -5,25 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
+    int x = 0;
+    String TAG = "firstpage";
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //final Intent intent = new Intent(this,mapactivity.class);
+        intent = new Intent(this,Login_activity.class);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
         //open intent
         new CountDownTimer(2000,1000) {
             @Override
             public void onTick(long l) {
-
+                Log.d(TAG, "onTick: "+x++);
             }
 
             @Override
             public void onFinish() {
-                //go to new intent
-                //startActivity(intent);
+
+                startActivity(intent);
             }
         };
     }
